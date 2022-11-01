709eae8f235eb3d02f48829cd0f1ffa830a7c8c3
ROMAN <83337336+romkagw@users.noreply.github.com>
2022-11-01 14:56:29 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
-$(function(){
+$(document).ready(function () {
+    var owl = $('.owl-carousel');
+  owl.owlCarousel({
+    items: 1,
+    loop:true,
+  });
 
+  $('.customNextBtn').click(function() {
+      owl.trigger('next.owl.carousel', [2000]);
+  })
+ 
+  $('.customPrevBtn').click(function() {
+      owl.trigger('prev.owl.carousel', [2000]);
+  })
 });
+
+
+
+
+
